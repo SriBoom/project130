c3fd152c3131965335350d3a0a7ae7255b20227d
SriBoom <74517999+SriBoom@users.noreply.github.com>
2022-02-05 11:58:33 -0800

    Add files via upload

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,5 +2,13 @@ import pandas as pd
 import csv
 
 df=pd.read_csv("merged_data.csv")
-print(df)
 
+del df["Star_name.1"]
+del df["Distance.1"]
+del df["Mass.1"]
+del df["Radius.1"]
+
+cleaned_data = df.dropna()
+print(cleaned_data)
+
+cleaned_data.to_csv("cleaned_data.csv")
